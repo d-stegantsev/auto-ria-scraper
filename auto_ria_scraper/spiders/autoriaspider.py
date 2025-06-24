04bcaea310f75d0916b667e42f24fc936341c73e
Dmytro Stehantsev <stegan296@gmail.com>
2025-06-24 09:48:57 +0300

    added basic parse_car_detail

--- auto_ria_scraper/spiders/autoriaspider.py
+++ auto_ria_scraper/spiders/autoriaspider.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 import scrapy
 
 
@@ -6,19 +8,26 @@ class AutoRiaSpider(scrapy.Spider):
     allowed_domains = ["auto.ria.com"]
     start_urls = ["https://auto.ria.com/uk/car/used/"]
 
+
     def parse(self, response):
-        cars = response.css(
-            "div.content-bar")
+        cars = response.css("div.content-bar")
 
         for car in cars:
-            title = car.css("a.address::attr(title)").get()
             url = car.css("a.address::attr(href)").get()
-
-            yield {
-                "title": title.strip() if title else None,
-                "url": response.urljoin(url) if url else None,
-            }
+            if url:
+                yield response.follow(url, callback=self.parse_car_detail)
 
         next_page = response.css("a.page-link.next::attr(href)").get()
         if next_page:
             yield response.follow(next_page, callback=self.parse)
+
+
+    def parse_car_detail(self, response):
+        item = {}
+
+        item["url"] = response.url
+        item["datetime_found"] = response.headers.get("Date", "").decode() or datetime.utcnow().isoformat()
+
+        item["title"] = response.css("h1.head::text").get()
+
+        yield item
